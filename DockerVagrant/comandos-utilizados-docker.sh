787d0f78a55184b5eeb1f78e6d9dc8647a113721
NestorMonroy <nestor.monroy.90@gmail.com>
2024-01-14 13:51:10 -0600

    [add] example jboss-docker

--- DockerVagrant/comandos-utilizados-docker.sh
+++ DockerVagrant/comandos-utilizados-docker.sh
@@ -28,3 +28,29 @@ docker inspect webl
 # Para  ver  los  contenedores corriendo: 
 docker ps  -a
 docker ps  -l
+
+# interactuar con los contenedores
+docker exec -ti webl /bin/bash
+docker stop nombre-contenedor
+docker start nombre-contenedor
+docker rm nombre-contenedor
+docker rm -f nombre-contenedor
+docker logs nombre-contenedor
+docker logs -f nombre-contenedor
+docker logs -t nombre-contenedor
+
+
+docker stop nombre-contenedor
+docker start nombre-contenedor
+docker restart nombre-contenedor
+docker pause nombre-contenedor
+docker unpause nombre-contenedor
+
+# Para ver las estadisticas de los contenedores y modificar en caliente la memoria de un contenedor
+docker stats
+docker update -m 200M --memory-swap -l  nombre-contenedor
+
+# Para natear puertos y llegar a los servicios de los contenedores
+docker run -dtiP --name web1 httpd
+docker run -dti -p 8085:80 --name web2 nginx
+iptables -t nat -L DOCKER -v -n
---
+++ DockerVagrant/jboss-docker/README.md
@@ -0,0 +1,18 @@
+# jboss-docker
+Este es un laboratorio para instalar Jboss 6.4 a través de Docker, con un contenedor basado en Centos 7 y optimizado.
+Añadimos los ficheros:
+
+standalone.xml -->Está configurado con el driver de mysql
+mysql-connector-java-5.1.27-bin.jar--> Driver de mysql para instalarlo en el servidor de Aplicaciones Jboss EAP 6.4
+module.xml--> Módulo para base de datos mysql configurado para Jboss EAP 6.4
+java.sh--> Para configurar las variables de entorno al contenedor de Jboss con la version de java jdk1.7.0_80
+
+En el proyecto se han utilizado los siguientes archivos que pasamos al contenedor a través del Dockerfile:
+-jboss-eap-6.4.0.zip
+-jdk-7u80-linux-x64.rpm
+
+# Contruimos la imagen
+
+```sh
+docker build --rm -f Dockerfile -t jboss-lab .
+```
