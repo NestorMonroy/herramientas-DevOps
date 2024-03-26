d95eff2a1f020fff0ffaac6c304519340ca5d8fc
nestor <nestor.monroy.90@gmail.com>
2024-03-26 16:02:31 -0600

    add node provision

--- AplicacionesWeb/appEcoCuentas/frontend/scripts/node.sh
+++ AplicacionesWeb/appEcoCuentas/frontend/scripts/node.sh
@@ -1,7 +1,28 @@
-sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=20
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt-get update && sudo apt-get install nodejs -y
-sudo npm install -g npm@10.5.0 @angular/cli
+#!/bin/bash
 
+# Define el nombre de usuario para el cual instalar NVM y Node.js
+
+# Función para instalar NVM y Node.js
+install_nvm_node() {
+  # Instalar NVM (Node Version Manager)
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+  # Cargar NVM
+  export NVM_DIR="/usr/local/nvm"
+  [ -s "\$NVM_DIR/nvm.sh" ] && \. "\$NVM_DIR/nvm.sh"
+  [ -s "\$NVM_DIR/bash_completion" ] && \. "\$NVM_DIR/bash_completion"
+
+  # Instalar la última versión de Node.js
+  nvm install node
+
+  # Establecer la versión instalada como la versión por defecto
+  nvm alias default node
+
+  # Verificar la instalación
+  echo "Node.js version:"
+  node -v
+  echo "npm version:"
+  npm -v
+}
+
+install_nvm_node
