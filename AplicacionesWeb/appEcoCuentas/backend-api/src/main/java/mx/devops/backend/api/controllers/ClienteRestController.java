aefcfff48c165dd195c51019577c3de26e65bc0e
nestor <nestor.monroy.90@gmail.com>
2024-03-30 22:52:36 -0600

    add CrossOrigin

--- AplicacionesWeb/appEcoCuentas/backend-api/src/main/java/mx/devops/backend/api/controllers/ClienteRestController.java
+++ AplicacionesWeb/appEcoCuentas/backend-api/src/main/java/mx/devops/backend/api/controllers/ClienteRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-@CrossOrigin(origins = { "http://localhost:4200" })
+@CrossOrigin(origins = {"http://10.11.12.2:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
